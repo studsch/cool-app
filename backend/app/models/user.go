c20920e971dbb6fe0f2b669ccd262bacd5806b49
studsch <113303183+studsch@users.noreply.github.com>
2023-10-27 23:19:23 +0700

    added validator for user close #44

--- backend/app/models/user.go
+++ backend/app/models/user.go
@@ -7,15 +7,15 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID
-	Phone        string
-	PasswordHash string
-	Name         string
-	Surname      string
+	ID           uuid.UUID `validate:"required,uuid"`
+	Phone        string    `validate:"e164"`
+	PasswordHash string    `validate:"required,lte=255"`
+	Name         string    `validate:"required,lte=80"`
+	Surname      string    `validate:"required,lte=80"`
 	DateOfBirth  time.Time
-	Gender       string
+	Gender       string `validate:"required,lte=10,oneof=Male Female"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
-	UserRole     string
+	UserRole     string `validate:"required,lte=10"`
 	Deleted      bool
 }
