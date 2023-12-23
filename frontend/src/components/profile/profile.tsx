534b301a993bf0b603f943dcac1825abd2e2eade
studsch <113303183+studsch@users.noreply.github.com>
2023-12-23 11:18:54 +0700

    added form for editing profile

--- frontend/src/components/profile/profile.tsx
+++ frontend/src/components/profile/profile.tsx
@@ -1,6 +1,42 @@
+"use client";
+
 import { Card } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "../ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const FormSchema = z.object({
+  name: z.string().min(1, { message: "Name must be at least 1 character." }),
+  description: z
+    .string()
+    .min(1, {
+      message: "Description must be at least 1 character.",
+    })
+    .max(180, {
+      message: "Description must not be longer than 180 characters.",
+    })
+    .optional(),
+});
 
 export interface ProfileProps {
   info: {
@@ -12,6 +48,14 @@ export interface ProfileProps {
 }
 
 export function Profile({ info }: ProfileProps) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
     <Card className="w-[600px]">
       {/* TODO: add image */}
@@ -34,7 +78,57 @@ export function Profile({ info }: ProfileProps) {
             </p>
           </div>
           <div className="grid gap-1 w-[200px]">
-            <Button className="text-white">Edit profile</Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="text-white">Edit profile</Button>
+              </DialogTrigger>
+              <DialogContent className="">
+                <DialogHeader>
+                  <DialogTitle>Edit your profile</DialogTitle>
+                </DialogHeader>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Your name" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Description</FormLabel>
+                          <FormControl>
+                            <Textarea
+                              placeholder="Tell us a little bit"
+                              className="resize-none"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <DialogFooter>
+                      <Button type="submit" className="text-white">
+                        Submit
+                      </Button>
+                    </DialogFooter>
+                  </form>
+                </Form>
+              </DialogContent>
+            </Dialog>
             <Button className="text-white">More action</Button>
           </div>
         </div>
