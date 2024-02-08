18c3b8f51b9dc10f6a48b3b1b2415e08ebba00cb
Stanko Dmitry <stanko.11@mail.ru>
2024-02-08 14:14:43 +0700

    Created slider multi slider. Close #118

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -2,12 +2,17 @@ import { AutoComplete } from "@/components/combobox/combobox";
 import SearchAutoComplete from "@/components/search-autocomplete/SearchAutoComplete";
 import SearchInput from "@/components/search-input/search-input";
 import { Select } from "@/components/ui/select";
+import Slider from "@/components/ui/sliders/Slider";
+import data from "@/test_data/slider/slider"; // тестовые данные для тестирования слайдера
+import RecentAvatarBlock from "@/components/avatarblock/recent-avatar-block";
 
 export default function page() {
   return (
-    <div className="flex justify-between">
-      <SearchAutoComplete />
-      <AutoComplete options={[]} emptyMessage={"Smt not found"} />
+    <div className=" justify-between">
+      {/* <SearchAutoComplete />
+      <AutoComplete options={[]} emptyMessage={"Smt not found"} /> */}
+      <Slider data={data} errMsg="No data"></Slider>
+      <RecentAvatarBlock></RecentAvatarBlock>
     </div>
   );
 }
