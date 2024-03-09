c00c02c4ba999d4ce4fc69b71b6fc1ded7b41276
valerii <krivets.valerii@yandex.ru>
2024-03-09 18:24:15 +0700

    changeavatarblock

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -3,19 +3,22 @@ import Slider from "@/components/ui/sliders/Slider";
 import { Recent } from "@/components/recent/recent";
 import MutualFriends from "@/components/mutual-friends/mutual-friends";
 // import data from "@/test_data/slider/slider"; // тестовые данные для тестирования слайдера
-// import users from "@/test_data/recent/users"; // для тестирования , слайдера и автаров
+// import users from "@/test_data/recent/users"; // для тестирования , слайдера и аватаров
 import users from "@/test_data/people/users"; // для теста общих друзей
 import People from "@/components/people/people";
 import DragDropPreview from "@/components/ui/dragdroppreview";
 import { Card } from "@/components/ui/card";
 import TopBar from "@/components/topbar/topbar";
 import PostCard from "@/components/card/card";
+import AvatarBlock from "@/components/avatarblock/avatarblock";
 
 export default function DragAndDrop() {
   return (
     <>
       <div>
         <TopBar />
+        <AvatarBlock avatarPosition="left" />
+        <AvatarBlock avatarPosition="right" />
       </div>
     </>
   );
--- frontend/src/components/avatarblock/avatarblock.tsx
+++ frontend/src/components/avatarblock/avatarblock.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default function AvatarBlock() {
+interface AvatarBlockProps {
+  avatarPosition: "left" | "right";
+}
+
+export default function AvatarBlock({ avatarPosition }: AvatarBlockProps) {
   return (
     <div className="flex items-center">
-      <div className="grid place-content-center">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
+      {avatarPosition === "left" && (
+        <div className="grid place-content-center">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      )}
+
       <div className="flex-col">
-        <div className="pl-4 grid place-content-left">
+        <div className="pl-4 pr-4 grid place-content-left">
           <div className="text-black">Morty Sanches</div>
         </div>
-        <div className="pl-4 grid place-content-left">
+        <div className="pl-4 pr-4 grid place-content-left">
           <div className="text-slate-400">14 September</div>
         </div>
       </div>
+
+      {avatarPosition === "right" && (
+        <div className="grid place-content-left">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      )}
     </div>
   );
 }
