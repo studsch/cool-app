c739c2940385a03f3e4686cc43b9bf3dd3986b1a
Stanko Dmitry <79981959+tim8842@users.noreply.github.com>
2024-04-04 18:10:25 +0700

    Close #170

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,3 +1,28 @@
-export {default} from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export const config = {matcher: ['/profile', '/protected/:path*']}
+export default async function middleware(
+    req: NextRequest,
+    event: NextFetchEvent
+) {
+    const token = await getToken({ req })
+    const isAuthenticated = !!token
+    if ((req.nextUrl.pathname.startsWith('/enter') || req.nextUrl.pathname.startsWith('/register')) && isAuthenticated) {
+        return NextResponse.redirect(new URL('/profile', req.url))
+    }
+
+
+    const authMiddleware = await withAuth({
+        pages: {
+            signIn: "/enter",
+        },
+    })
+
+    // @ts-expect-error
+    return authMiddleware(req, event)
+}
+
+export const config = {
+    matcher: ['/profile', '/register/:path*', "/enter"],
+}
