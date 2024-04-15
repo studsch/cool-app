ef4ebacd4e18d90cec1ca81e8fb87291c4621d9e
Dmitry S <stanko.11@mail.ru>
2024-04-15 23:40:46 +0700

    add post and users data execute

--- rec-sys/src/data_execution/posts.py
+++ rec-sys/src/data_execution/posts.py
@@ -4,9 +4,14 @@ import psycopg2
 import pandas as pd
 import os
 
+#CONFIG interval
+INTERVAL = '1 months'
+TAGS_LIMIT = 2
+#CONFIG most popular tegs
+
 @click.command()
 @click.option("--out", "-o", "output", default="data", help="path where need save posts: '../data'", type=click.Path(exists=True), required=True)
-@click.option("--col", "-c", "columns", default="id user_id description created_at", help="columns to execute: 'id user_id description created_at image_urls'", required=True)
+@click.option("--col", "-c", "columns", default="id user_id description tag_ids created_at", help="columns to execute: 'id user_id description created_at image_urls'", required=True)
 @click.option("--table", "-t", "table", default="post", help="name of table: 'posts'", required=True)
 def execute_useres(output, columns, table):
     data = None
@@ -18,7 +23,12 @@ def execute_useres(output, columns, table):
         cursor = conn.cursor()
         columns = columns.split()
         str_select = ", ".join(columns)
-        cursor.execute(f"""SELECT {str_select} FROM {table};""") # "rel" is short for relation.
+        str_select = str_select.replace("created_at", "EXTRACT(EPOCH FROM created_at )")
+        cursor.execute(f"""WITH top_tags as (SELECT post_id, ARRAY_AGG(tag_id) as tag_ids from post_tags WHERE tag_id IN (SELECT tag_id from (SELECT tag_id, count(distinct (tag_id, post_id)) as unique_tags
+	FROM post_tags
+	GROUP BY tag_id
+	ORDER By unique_tags DESC limit {TAGS_LIMIT})) GROUP BY post_id)
+SELECT {str_select} FROM {table} JOIN top_tags tt ON tt.post_id = id WHERE created_at >= CURRENT_DATE - INTERVAL '{INTERVAL}'""")
         users = pd.DataFrame(cursor.fetchall(), columns=columns) # A list() of tables.
         users.to_csv(os.path.join(output, "posts.csv"), index=False)
         conn.close()
