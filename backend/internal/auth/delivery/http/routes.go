ead25f8c738dadf967a8ce77631fbd3d76f70159
studsch <113303183+studsch@users.noreply.github.com>
2024-04-29 11:43:19 +0700

    fixed renew token

--- backend/internal/auth/delivery/http/routes.go
+++ backend/internal/auth/delivery/http/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/studsch/cool-app/backend/pkg/utils"
 
 	"github.com/gofiber/fiber/v2"
+
 	"github.com/studsch/cool-app/backend/internal/auth"
 	"github.com/studsch/cool-app/backend/internal/middleware"
 )
@@ -17,19 +18,21 @@ func MapAuthRoutes(
 	authGroup.Post("/register", h.Register())
 	authGroup.Post("/login", h.Login())
 	authGroup.Get("/search/user", h.Search())
-	authGroup.Use(mw.AuthJWTMiddleware())
 	authGroup.Post("/token/renew", h.RenewTokens())
+	authGroup.Use(mw.AuthJWTMiddleware())
 	authGroup.Post("/logout", h.Logout())
 	authGroup.Post("/avatar", h.UploadAvatar())
 	authGroup.Put("/:id", mw.OwnerMiddleware(), h.Update())
-	authGroup.Get("/health", func(c *fiber.Ctx) error {
-		q := c.Locals("user")
-		u, e := utils.GetUserFromCtx(c.UserContext())
-		if e != nil {
-			fmt.Println("qweqwe")
-		}
-		fmt.Println(u)
-		fmt.Println(q)
-		return c.SendStatus(fiber.StatusOK)
-	})
+	authGroup.Get(
+		"/health", func(c *fiber.Ctx) error {
+			q := c.Locals("user")
+			u, e := utils.GetUserFromCtx(c.UserContext())
+			if e != nil {
+				fmt.Println("qweqwe")
+			}
+			fmt.Println(u)
+			fmt.Println(q)
+			return c.SendStatus(fiber.StatusOK)
+		},
+	)
 }
